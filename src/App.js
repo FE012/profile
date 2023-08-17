d358db178f45e9b0df0496fedb2a84b945c50a86
FE012 <twte96@gmail.com>
2023-08-17 18:27:17 +0900

    chore: React-Router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Headers from "./components/Headers";
+import Home from "./page/Home";
+import Resume from "./page/Resume";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Headers></Headers>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
