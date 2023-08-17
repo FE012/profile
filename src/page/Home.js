8a3e5cf349c9012f8ab1b60d31d98401e39b8a73
FE012 <twte96@gmail.com>
2023-08-17 18:26:50 +0900

    chore: 기본세팅

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Banner = styled.div``;
+const Profile = styled.img``;
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <Banner>
+        배너
+        <Profile></Profile>
+      </Banner>
+    </Wrapper>
+  );
 }
 
 export default Home;
