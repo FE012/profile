313039b236d5f080bf7bd715f25a7f926c6a8be3
FE012 <twte96@gmail.com>
2023-08-17 21:24:01 +0900

    chore: Home 확인

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,19 +1,7 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
-
-const Banner = styled.div``;
-const Profile = styled.img``;
-
 function Home() {
-  return (
-    <Wrapper>
-      <Banner>
-        배너
-        <Profile></Profile>
-      </Banner>
-    </Wrapper>
-  );
+  return <>Home</>;
 }
 
 export default Home;
