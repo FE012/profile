a9527b1ccc08615b80083881576b45e926223d7b
FE012 <twte96@gmail.com>
2023-08-18 14:46:47 +0900

    feat: 기술스택에 들어갈 data

---
+++ src/data.js/skillsData.js
@@ -0,0 +1,47 @@
+import reactLogo from "../assests/image/reactLogo.png";
+import reactQueryLogo from "../assests/image/reactQueryLogo.png";
+import recoilLogo from "../assests/image/recoilLogo.png";
+import jsLogo from "../assests/image/jsLogo.png";
+
+export const skillsData = [
+  {
+    id: 0,
+    logoImg: reactLogo,
+    name: "React",
+    content:
+      "SPA 구현에 필요한 React Router를 이해하고, 웹 페이지에 SPA 기술을 적용할 수 있습니다. 컴포넌트 재사용에 익숙하고 기본적인 hook 사용에 능숙합니다.",
+  },
+  {
+    id: 1,
+    logoImg: reactQueryLogo,
+    name: "React Query",
+    content: "useQuery를 이용해 로딩 화면을 구현할 수 있습니다.",
+  },
+  {
+    id: 2,
+    logoImg: recoilLogo,
+    name: "Recoil",
+    content:
+      "복잡한 redux와 달리 간단한 recoil을 사용하여 전역 상태를 관리 할 수 있습니다.",
+  },
+  {
+    id: 3,
+    logoImg: jsLogo,
+    name: "JavaScript",
+    content: "useQuery를 이용해 로딩 화면을 구현할 수 있습니다.",
+  },
+  {
+    id: 4,
+    logoImg: jsLogo,
+    name: "CSS",
+    content:
+      " css 선택자와 flex 사용에 능숙하며, 웹을 반응형으로 만들수 있습니다.",
+  },
+  {
+    id: 5,
+    logoImg: jsLogo,
+    name: "HTML",
+    content:
+      "  div 태그를 사용하기보다 상황에 맞는 시멘틱 태그를 사용하고, 시멘틱 태그를 사용해야 하는 이유를 알고 있습니다.",
+  },
+];
