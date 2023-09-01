c9b2d5dc05fe17301bd802bce649209418c22495
FE012 <twte96@gmail.com>
2023-09-01 16:25:37 +0900

    fix: css import 에러나는거 고침

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Headers from "./components/Headers";
 import Home from "./page/Home";
 import Resume from "./page/Resume";
@@ -7,7 +6,7 @@ import Resume from "./page/Resume";
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Headers></Headers>
+      <Headers />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/resume" element={<Resume />}></Route>
