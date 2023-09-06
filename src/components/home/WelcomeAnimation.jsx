fcad24bf27bab629f80ddf3620c0db6c1ca4f3a1
FE012 <twte96@gmail.com>
2023-09-06 22:57:52 +0900

    feat: Home page 애니메이션 구현

---
+++ src/components/home/WelcomeAnimation.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
+import { Link } from "react-router-dom";
+
+const Section = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.bgColor};
+  h1 {
+    margin-bottom: 10px;
+    color: ${(props) => props.theme.textColor};
+  }
+`;
+
+const NameBox = styled.div`
+  text-align: center;
+  font-weight: var(--bold-weight);
+  font-size: var(--big-font);
+  margin-bottom: 15px;
+  @media (max-width: 720px) {
+    font-size: var(--title-font);
+  }
+`;
+
+const TypewriterWithCustomStyle = styled(Typewriter)`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const LinkBox = styled.div`
+  font-size: var(--main-font);
+  font-weight: var(--bold-weight);
+  :hover {
+    color: var(--point-color);
+  }
+  @media (max-width: 680px) {
+    font-size: var(--small-font);
+  }
+`;
+
+function WelcomeAnimation() {
+  return (
+    <Section>
+      <NameBox>
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2.0 }}
+          style={{ color: "var(--point-color)" }}
+        >
+          <h1>웹을 통해 아이디어를 현실로 만드는</h1>
+          <TypewriterWithCustomStyle
+            words={["", "프론트엔드 개발자 김예본입니다."]}
+            cursor
+            cursorStyle="|"
+            delaySpeed={500}
+            typeSpeed={200}
+          />
+        </motion.div>
+      </NameBox>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+      >
+        <LinkBox>
+          <Link to="/resume">Resume</Link>
+        </LinkBox>
+      </motion.div>
+    </Section>
+  );
+}
+
+export default WelcomeAnimation;
