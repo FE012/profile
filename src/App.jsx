22ab05975343d6f930559d82b784d9cf5a784d5d
FE012 <twte96@gmail.com>
2023-09-07 18:27:23 +0900

    feat: useState로 라이트/다크모드 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Headers from "./components/Headers";
 import GlobalStyle from "./GlobalStyle";
 import Home from "./page/Home";
 import Resume from "./page/Resume";
 import { darkTheme, lightTheme } from "./theme";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+
+const Button = styled.button`
+  position: fixed;
+  bottom: 3rem;
+  right: 3rem;
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.toggleBgColor};
+  color: ${(props) => props.theme.accentColor};
+`;
 
 function App() {
   const [isDark, setIsDark] = useState(false);
@@ -16,7 +32,13 @@ function App() {
       <GlobalStyle />
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Headers />
-        <button onClick={toggleDark}>Toggle Button</button>
+        <Button onClick={toggleDark}>
+          {isDark ? (
+            <FontAwesomeIcon icon={faMoon} size="2xl" />
+          ) : (
+            <FontAwesomeIcon icon={faSun} size="2xl" />
+          )}
+        </Button>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/resume" element={<Resume />}></Route>
