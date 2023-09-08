24f17d253f7b8cdf00a7ce4701af7ab97e4050c5
FE012 <twte96@gmail.com>
2023-09-08 22:18:16 +0900

    fix: Data 내용 수정

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,6 +1,7 @@
 import projectImg from "../assests/image/projectImg.png";
 import coinImg from "../assests/image/coinImg.png";
 import toDoImg from "../assests/image/toDoImg.png";
+import NomflixImg from "../assests/image/nomflixImg.png";
 
 export const projectData = [
   {
@@ -11,33 +12,74 @@ export const projectData = [
     pageView: "https://www.notion.so/fa7759d7bb2c4f38bcc3bdede4d0e2d6",
     img: projectImg,
     name: "우리만의 공간 azit [팀프로젝트]",
-    period: "2023.01 ~ 2023.02 (4주, 7인)",
-    content:
-      "코드스테이츠 과정에서 팀 프로젝트로 제작된 같은 취미를 찾아서 취미를 공유하는 플랫폼입니다. 프론트엔드 팀원으로서 유저 마이페이지, 프로필 수정페이지, 유저 신고페이지를 담당해 개발하였습니다.",
-    skills: "React, Styled-components, Axios, React-query, Redux",
+    summary:
+      "같은 취미를 찾아서 취미를 공유하고, 모임활동을 할 수 있는 플랫폼입니다.",
+    period: "2023.01 ~ 2023.02 (4주, 7인) 프론트엔드 담당",
+    skills:
+      "#React, #Styled-components, #React-query, #Axios, #Redux, #AWS(S3)",
+    planning: [
+      "notion으로 화면정의서를 작성하여 기획",
+      "초기 프로토타입은 Figma를 사용해 디자인",
+    ],
+    realization: [
+      "유저 마이페이지, 프로필 수정페이지, 유저 신고페이지 개발 담당",
+      "수정 상태에 따라 버튼의 활성화 여부를 바꾸는 것이 적합하다 생각해서 input을 제어 컴포넌트로 구현했습니다.",
+      "비동기 데이터 관리의 불편함을 해소하기 위해 React-query를 사용했습니다.",
+      "React-query로 로딩 상태에 따른 처리를 하여 사용자에게 빈 화면을 보여주지 않고, 로딩 컴포넌트를 보여줌으로써 UX를 향상시켰습니다.",
+      "팀원들과 같이 사용할 수 있도록 공통 컴포넌트를 제작했습니다.",
+      "버튼 컴포넌트를 제작해서 프로젝트의 기본적인 UI를 통일하기 쉽도록 구현 했습니다.",
+    ],
   },
   {
     id: 1,
+    github: "https://github.com/FE012/react_nomflix",
+    vlog: "",
+    link: "https://fe012.github.io/react_nomflix/",
+    pageView: "",
+    img: NomflixImg,
+    name: "Nomflix [개인]",
+    summary: "실제 넷플릭스 사이트를 클론코딩 했습니다.",
+    period: "2023.5 (2주)",
+    skills:
+      "#React, #TypeScript, #React-query, #Styled-components, #Framer Motion",
+    planning: [],
+    realization: [
+      "Framer Motion 을 사용한 로고, 검색영역 등 애니메이션 구현",
+      "React-query를 사용해 영화정보 API 불러오기 구현",
+      "영화 슬라이드 및 모달창  Hover, Click 애니메이션 구현",
+      "영화 검색 기능 구현",
+      "TypeScript 적용",
+    ],
+  },
+  {
+    id: 2,
     github: "https://github.com/FE012/react_crypto-tracker",
     vlog: "",
     link: "https://fe012.github.io/react_crypto-tracker/",
     img: coinImg,
-    name: "coin trackers [개인]",
-    period: "2023.04",
-    content:
-      "실시간으로 암호화폐의 시세를 차트로 조회할 수 있는 웹을 만들었습니다. 암호화폐 목록들을 보여줄때, react-query의 useInfiniteQuery hook을 사용한 Infinite Scroll 기능을 추가했으며, 다크/라이트모드 기능도 추가했습니다.",
-    skills: "React, TypeScript, React-query, Styled-components, ApexCharts",
+    name: "Coin tracker [개인]",
+    summary:
+      "다양한 암호화폐들의 정보와 실시간 거래가격을 볼 수 있는 사이트를 만들었습니다. ",
+    period: "2023.05 (1주)",
+    skills: "#React, #TypeScript, #React-query, #Styled-components, #Recoil",
+    realization: [
+      "코인파프리카 API를 사용하여, 코인 정보 및 실시간 가격 불러오기 (React-query)",
+      "Styled-components + recoil을 이용한 theme으로 라이트/다크모드 구현",
+      "ApexChart 라이브러리를 이용한 실시간 시세조회 Chart 구현",
+      "React-router-dom을 사용하여 Tab UI 구현",
+    ],
   },
   {
-    id: 2,
+    id: 3,
     github: "https://github.com/FE012/recoil-to-do",
     vlog: "",
     link: "https://fe012.github.io/recoil-to-do/",
     img: toDoImg,
-    name: "recoil todo [개인]",
-    period: "2023.05",
-    content:
-      "recoil을 사용한 To Do List를 만들었습니다. 다양한 카테고리를 생성할 수 있고, CRUD기능을 구현했습니다.",
-    skills: "React, TypeScript, Recoil, Styled-components",
+    name: "To Do List [개인]",
+    summary:
+      " Recoil을 사용한 To Do List를 만들었습니다. 사용자가 원하는 카테고리를 추가할 수 있습니다.",
+    period: "2023.04(1주)",
+    skills: "#React, #TypeScript, #Recoil, #Styled-components",
+    realization: ["Recoil을 사용한 CRUD 구현", "분류 카테고리 추가기능 구현"],
   },
 ];
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,57 +1,56 @@
 import reactLogo from "../assests/image/reactLogo.png";
-import reactQueryLogo from "../assests/image/reactQueryLogo.png";
 import recoilLogo from "../assests/image/recoilLogo.png";
 import jsLogo from "../assests/image/jsLogo.png";
 import htmlLogo from "../assests/image/htmlLogo.png";
 import cssLogo from "../assests/image/cssLogo.png";
 import reduxLogo from "../assests/image/reduxLogo.png";
-
+import styledComponentsLogo from "../assests/image/styledComponentsLogo.png";
 export const skillsData = [
   {
     id: 0,
-    logoImg: reactLogo,
-    name: "React",
-    content:
-      "SPA 구현에 필요한 React Router를 이해하고, 웹 페이지에 SPA 기술을 적용할 수 있습니다. 컴포넌트 재사용에 익숙하고 기본적인 hook 사용에 능숙합니다.",
-  },
-  {
-    id: 1,
-    logoImg: reduxLogo,
-    name: "Redux",
-    content: "Redux 입니다",
-  },
-  {
-    id: 2,
-    logoImg: reactQueryLogo,
-    name: "React Query",
-    content: "useQuery를 이용해 로딩 화면을 구현할 수 있습니다.",
-  },
-  {
-    id: 3,
-    logoImg: recoilLogo,
-    name: "Recoil",
-    content:
-      "복잡한 redux와 달리 간단한 recoil을 사용하여 전역 상태를 관리 할 수 있습니다.",
-  },
-  {
-    id: 4,
     logoImg: jsLogo,
     name: "JavaScript",
     content:
-      " es6 문법을 사용하는 것에 익숙하고 JavaScript의 동작 원리를 이해하고 있습니다.",
+      " 구조분해할당, spread 구문 등 ES6 문법 사용과 주요 Array 내장 메서드에 사용에 익숙합니다.",
+  },
+  {
+    id: 1,
+    logoImg: reactLogo,
+    name: "React",
+    content:
+      "컴포넌트의 재사용과 유지보수의 향상을 위해 컴포넌트 단위로 코드를 작성 하며, useState, useEffect, useRef 같은 기본적인 hook 사용에 능숙합니다. ",
+  },
+  {
+    id: 2,
+    logoImg: recoilLogo,
+    name: "Recoil",
+    content: "recoil을 사용하여 간단하게 전역 상태를 관리 할 수 있습니다.",
+  },
+  {
+    id: 3,
+    logoImg: reduxLogo,
+    name: "Redux",
+    content: "flux 패턴의 기본적인 동작 원리를 이해하고 있습니다.",
+  },
+  {
+    id: 4,
+    logoImg: styledComponentsLogo,
+    name: "Styled-components",
+    content:
+      "props를 사용해 조건부 렌더링을 할 수 있는 장점을 적극적으로 활용하며, ThemeProvider로 공통 스타일 속성 관리를 할 수 있습니다.",
   },
   {
     id: 5,
-    logoImg: cssLogo,
-    name: "CSS",
+    logoImg: htmlLogo,
+    name: "HTML5",
     content:
-      " css 선택자와 flex 사용에 능숙하며, 웹을 반응형으로 만들수 있습니다.",
+      "div 태그를 사용하기 보다 때에 맞는 Semantic 태그를 사용해 웹 접근성을 향상시키는 마크업을 작성합니다.",
   },
   {
-    id: 6,
-    logoImg: htmlLogo,
-    name: "HTML",
+    id: 4,
+    logoImg: cssLogo,
+    name: "CSS3",
     content:
-      "  div 태그를 사용하기보다 상황에 맞는 시멘틱 태그를 사용하고, 시멘틱 태그를 사용해야 하는 이유를 알고 있습니다.",
+      " 적절한 선택자와 css 변수를 사용할 수 있고, flexBox, position 속성을 활용해 레이아웃을 구성할 수 있습니다. 웹을 반응형으로 만들 수 있습니다.",
   },
 ];
