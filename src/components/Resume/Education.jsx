149eaee4ba2d3fb55c8d0cbbde443e652e420fe5
FE012 <twte96@gmail.com>
2023-09-11 07:36:47 +0900

    chore: Education 컴포넌트 생성

--- src/components/Resume/Education.jsx
+++ src/components/Resume/Education.jsx
@@ -1,10 +1,80 @@
+import React from "react";
 import styled from "styled-components";
-import { Title } from "./Introduce";
+import { Title } from "../Resume/Title";
+import { EducationData } from "../../data/EducationData";
 
-const Container = styled.div``;
+const EducationBox = styled.article``;
+
+const Ul = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 0.1fr;
+  gap: 10px;
+  li {
+    padding: 5px 5px;
+    display: flex;
+    align-items: center;
+    white-space: nowrap;
+    background-color: ${(props) => props.theme.greyBoxColor};
+    font-size: var(--main-font);
+    a {
+      padding-bottom: 2px;
+      font-weight: var(--bold-weight);
+    }
+  }
+  /* 제목-과정,기간,수료 */
+  li:first-of-type,
+  li:nth-of-type(2),
+  li:nth-of-type(3) {
+    font-weight: var(--bold-weight);
+    background-color: ${(props) => props.theme.toggleBgColor};
+    border-radius: 5px;
+  }
+  /* 내용 */
+  li:nth-child(3n) {
+    display: flex;
+    justify-content: center;
+  }
+`;
 
 function Education() {
-  return <Title>🎓 교육</Title>;
+  return (
+    <EducationBox>
+      <Title>🎓 교육</Title>
+      <Ul>
+        <li>과정</li>
+        <li>기간</li>
+        <li>수료증</li>
+        {EducationData.map((edu) => (
+          <>
+            <li>
+              <a href={edu.site} target="_blank" rel="noreferrer">
+                {edu.course}
+              </a>
+            </li>
+            <li>
+              {edu.period}&nbsp;
+              {edu.id !== 4 ? (
+                <a href={edu.vlog} target="_blank" rel="noreferrer">
+                  📎블로깅
+                </a>
+              ) : (
+                ""
+              )}
+            </li>
+            <li>
+              {edu.id !== 4 ? (
+                <a href={edu.certificates} target="_blank" rel="noreferrer">
+                  📎
+                </a>
+              ) : (
+                ""
+              )}
+            </li>
+          </>
+        ))}
+      </Ul>
+    </EducationBox>
+  );
 }
 
 export default Education;
