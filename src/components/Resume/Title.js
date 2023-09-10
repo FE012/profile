82690345301b89e20433c1035470906cfb9228df
FE012 <twte96@gmail.com>
2023-09-11 07:33:46 +0900

    chore: 스타일 컴포넌트 전역 컴포넌트 파일생성

---
+++ src/components/Resume/Title.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// 전역으로 쓰는 제목 스타일 컴포넌트
+export const Title = styled.h2`
+  padding-bottom: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: var(--title-font);
+  border-bottom: 2px solid var(--main-color);
+`;
