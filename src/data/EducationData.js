be5131f89238b61d37f2514fbc10b4127c50d86e
FE012 <twte96@gmail.com>
2023-09-11 07:41:39 +0900

    feat: Education Data 생성

---
+++ src/data/EducationData.js
@@ -0,0 +1,40 @@
+export const EducationData = [
+  {
+    id: 0,
+    course: "Nomad Coders Challenge - React 2주 챌린지",
+    period: "2023.05 (2주)",
+    vlog: "https://velog.io/@developer09/series/%EB%85%B8%EB%A7%88%EB%93%9C%EC%BD%94%EB%8D%94-ReactJS-%EB%A7%88%EC%8A%A4%ED%84%B0%ED%81%B4%EB%9E%98%EC%8A%A4",
+    certificates:
+      "https://drive.google.com/file/d/1FRwFA63cfvWNDCYloSagW-KlKBxERGhq/view?usp=sharing",
+  },
+  {
+    id: 1,
+    course: "Nomad Coders Challenge - 바닐라JS 2주 챌린지",
+    period: "2023.04 (2주)",
+    vlog: "https://velog.io/@developer09/series/%EB%85%B8%EB%A7%88%EB%93%9C%EC%BD%94%EB%8D%94-%EB%B0%94%EB%8B%90%EB%9D%BCJS%EB%A1%9C-%ED%81%AC%EB%A1%AC%EC%95%B1-%EB%A7%8C%EB%93%A4%EA%B8%B0",
+    certificates:
+      "https://drive.google.com/file/d/1FF9mD57Ib0gyAqTBseMZgjgRV6a6CGr2/view?usp=drive_link",
+  },
+  {
+    id: 2,
+    course: "Nomad Coders Challenge - 코코아 클론 2주 챌린지",
+    period: "2023.03 (2주)",
+    vlog: "https://velog.io/@developer09/series/%EB%85%B8%EB%A7%88%EB%93%9C%EC%BD%94%EB%8D%94-%EC%BD%94%EC%BD%94%EC%95%84%ED%86%A1-%ED%81%B4%EB%A1%A0-%EC%BD%94%EB%94%A9",
+    certificates:
+      "https://drive.google.com/file/d/1K0SyM8X5YB7P65UiDIUdes5fXVdKgIR2/view?usp=drive_link",
+  },
+  {
+    id: 3,
+    course: "코드스테이츠 - 프론트엔드 개발자 부트캠프 (📎커리큘럼)",
+    period: "2022.08 - 2023.02 (6개월)",
+    vlog: "https://velog.io/@developer09/series/TIL",
+    site: "https://www.codestates.com/course/frontend-engineering?gclid=CjwKCAjw5P2aBhAlEiwAAdY7dMdI6OKTI-9GmijFvfouSMqLb160-nc5GuQYxtocIQ2ipyjhhoOcuxoCTTIQAvD_BwE",
+    certificates:
+      "https://drive.google.com/file/d/1s6T5VGguIKRB-w0tM4kSo6nfJ5Tb5Pu4/view?usp=sharing",
+  },
+  {
+    id: 4,
+    course: "울산대학교 피아노과 졸업",
+    period: "2015.03 - 2020.02",
+  },
+];
