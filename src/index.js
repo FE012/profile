6d004a26c3de9ccbbac6d03914ceee4e8efc8dcd
FE012 <twte96@gmail.com>
2023-09-11 16:40:41 +0900

    chore: BrowerRouter  옮기고 Router 컴포넌트화 시킴. Recoil세팅

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./components/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+        <Router />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
