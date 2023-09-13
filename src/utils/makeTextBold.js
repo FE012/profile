018ae943ca01b88c44076a96944b3ef80b36c979
FE012 <twte96@gmail.com>
2023-09-13 23:43:26 +0900

    feat: 특정 텍스트 bod 기능 컴포넌트 구현

--- src/utils/makeTextBold.js
+++
@@ -1,7 +0,0 @@
-export function makeTextBold(inputText, targetTexts) {
-  let result = inputText;
-  targetTexts.forEach((targetText) => {
-    result = result.split(targetText).join(`<strong>${targetText}</strong>`);
-  });
-  return result;
-}
