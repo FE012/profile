c5993d4ac26e10402364c171a38edfa66003e5f7
FE012 <twte96@gmail.com>
2023-09-13 23:37:35 +0900

    chore: BrowserRouter 위치 옮김

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./components/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
-        <Router />
-      </BrowserRouter>
+      <App />
     </RecoilRoot>
   </React.StrictMode>
 );
