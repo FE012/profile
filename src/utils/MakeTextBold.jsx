bac04f333c80bd1907af65d4a6a6c37a13082925
FE012 <twte96@gmail.com>
2023-09-13 23:42:58 +0900

    feat: 특정 글씨 bold 기능 컴포넌트 구현

---
+++ src/utils/MakeTextBold.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MakeTextBold({ children, className, as = "p" }) {
+  const pattern = /(\*\*.*?\*\*)/;
+
+  const text = children;
+  const parts = text.split(pattern).filter((x) => x.length > 0);
+
+  const elements = parts.map((part, index) => {
+    if (part.match(pattern)) {
+      return <strong key={index}>{part.slice(2, -2)}</strong>;
+    } else {
+      return <span key={index}>{part}</span>;
+    }
+  });
+
+  return React.createElement(as, { className }, elements);
+}
+
+export default MakeTextBold;
